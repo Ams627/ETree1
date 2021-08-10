2b2e7bfa1c644ab55199133981f622484d6de5fa
Adrian Sims <adriansims62781@gmail.com>
2021-08-10 09:56:05 +0200

    Add project files.

---
+++ ETree1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace ETree1
+{
+    class Program
+    {
+        private static void Print(string s)
+        {
+            Console.WriteLine(s);
+        }
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var str1 = Expression.Constant("Hello", typeof(string));
+                var mi = typeof(Program).GetMethod("Print", BindingFlags.Static | BindingFlags.NonPublic);
+                var lambda = Expression.Lambda(Expression.Call(mi, str1));
+                Action res = (Action)lambda.Compile();
+                res();
+            }
+            catch (Exception ex)
+            {
+                var fullname = System.Reflection.Assembly.GetEntryAssembly().Location;
+                var progname = Path.GetFileNameWithoutExtension(fullname);
+                Console.Error.WriteLine($"{progname} Error: {ex.Message}");
+            }
+
+        }
+    }
+}
